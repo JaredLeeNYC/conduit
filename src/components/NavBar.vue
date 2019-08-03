45265dfb09039a4e835ea76f14f01b9474bdedac
Jared Lee <lzbstar@gmail.com>
2019-08-03 16:56:32 +0800

    commit settings and logout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <router-link to="/" class="navbar-brand">conduit</router-link>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <router-link
+            to="/"
+            class="nav-link"
+            exact-active-class="nav-link active"
+            >Home</router-link
+          >
+        </li>
+        <li v-if="username" class="nav-item">
+          <router-link
+            to="/editor"
+            class="nav-link"
+            exact-active-class="nav-link active"
+          >
+            <i class="ion-compose"></i>&nbsp;New Post
+          </router-link>
+        </li>
+        <li v-if="username" class="nav-item">
+          <router-link
+            to="/settings"
+            class="nav-link"
+            exact-active-class="nav-link active"
+          >
+            <i class="ion-compose"></i>&nbsp;Settings
+          </router-link>
+        </li>
+        <li v-if="username == null" class="nav-item">
+          <router-link
+            to="/login"
+            class="nav-link"
+            exact-active-class="nav-link active"
+          >
+            <i class="ion-compose"></i>&nbsp;Sign in
+          </router-link>
+        </li>
+        <li v-if="username == null" class="nav-item">
+          <router-link
+            to="/register"
+            class="nav-link"
+            exact-active-class="nav-link active"
+          >
+            <i class="ion-compose"></i>&nbsp;Sign up
+          </router-link>
+        </li>
+        <li v-if="username" class="nav-item">
+          <router-link
+            to="`/@${username}`"
+            class="nav-link"
+            exact-active-class="nav-link active"
+          >
+            <i class="ion-compose"></i>
+            &nbsp;{{ username }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  computed: {
+    username() {
+      return this.$store.getters["users/username"];
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                 <a class="nav-link disabled" href>Your Feed</a>
+                <a class="nav-link disabled" href>Your Feed</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link active" href>Global Feed</a>
