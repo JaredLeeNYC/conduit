0ed3d937d352b2e22426d7984ee59439010ea35f
Jared Lee <lzbstar@gmail.com>
2019-08-18 00:47:23 +0800

    new article

--- src/views/Create-Edit-Article.vue
+++ src/views/Create-Edit-Article.vue
@@ -10,6 +10,7 @@
                   type="text"
                   class="form-control form-control-lg"
                   placeholder="Article Title"
+                  v-model="title"
                 />
               </fieldset>
               <fieldset class="form-group">
@@ -17,6 +18,7 @@
                   type="text"
                   class="form-control"
                   placeholder="What's this article about?"
+                  v-model="description"
                 />
               </fieldset>
               <fieldset class="form-group">
@@ -24,6 +26,7 @@
                   class="form-control"
                   rows="8"
                   placeholder="Write your article (in markdown)"
+                  v-model="body"
                 ></textarea>
               </fieldset>
               <fieldset class="form-group">
@@ -31,12 +34,22 @@
                   type="text"
                   class="form-control"
                   placeholder="Enter tags"
+                  v-model="tag"
+                  @keyup.enter="addTag()"
                 />
-                <div class="tag-list"></div>
+                <div class="tag-list">
+                  <span
+                    class="tag-default tag-pill"
+                    v-for="(v, k) in tagList"
+                    :key="k"
+                    >{{ v }}</span
+                  >
+                </div>
               </fieldset>
               <button
                 class="btn btn-lg pull-xs-right btn-primary"
                 type="button"
+                @click="submit()"
               >
                 Publish Article
               </button>
@@ -47,3 +60,38 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      description: "",
+      body: "",
+      tag: "",
+      tagList: []
+    };
+  },
+  methods: {
+    addTag() {
+      this.tagList.push(this.tag);
+      this.tag = "";
+    },
+    submit() {
+      this.$store
+        .dispatch("article/create", {
+          title: this.title,
+          description: this.description,
+          body: this.body,
+          tagList: this.tagList
+        })
+        .then(() => {
+          // #todo, nice toast and no redirect
+          this.$router.push({ name: "article" });
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
